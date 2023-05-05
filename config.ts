76a916deac66bae370d3a28302f412da9075d552
Durmy <121383483+DurmyDurman@users.noreply.github.com>
2023-05-05 13:02:45 +0200

    Init

---
+++ config.ts
@@ -0,0 +1,61 @@
+import path from "path";
+
+export const ARCGIS_URL = 'https://www.datalk.cz/data.json';
+export const ARCGIS_FILE = path.resolve(__dirname, 'data/arcgis.json');
+
+export const DTO_FILE = path.resolve(__dirname, 'data/dto.json');
+
+export const LKOD_URL = process.env.URL || process.env.VERCEL_URL || 'https://open.datalk.cz'; //umisteni LKOD
+export const LKOD_DIR = path.resolve(__dirname, 'public');
+export const LKOD_FILE = path.resolve(__dirname, 'public/katalog.jsonld');
+
+export const META_LKOD = {
+	name_cz: "Katalog otevřených dat Libereckého kraje",
+	description_cz: "Otevřená data Libereckého kraje. Datové sady jsou ve strojově čitelných formátech, volně přístupné k libovolným (legálním) účelům využití, bez licenčních omezení.",
+	provider: "https://rpp-opendata.egon.gov.cz/odrpp/zdroj/orgán-veřejné-moci/70889546",
+	homepage: "https://datalk.cz",
+	contact_type: 'Organizace',
+	contact_name: 'ARR - Agentura regionálního rozvoje, spol. s r.o.',
+	contact_email: 'k.tichy@arr-nisa.cz',
+	themes: {
+		'Obecná charakteristika': ['REGI', 'GOVE']
+		'Kraj jako instituce': ['GOVE', 'ECON']
+		'Zaměstnanost a pracovní trh': ['ECON','SOCI']
+		'Podníkání, výzkum, vývoj a inovace': ['ECON', 'TECH']
+		'Cestovní ruch': ['ECON','EDUC']
+		'Sport': ['SOCI', 'EDUC']
+		'Kultura a kulturní dědicství':['EDUC']
+		'Doprava': ['TRAN']
+		'Technická infrastruktura': ['TECH', 'TRAN']
+		'Vzdělání': ['SOCI', 'EDUC']
+		'Zdravotníctví a zdraví': ['SOCI' ,'HEAL']
+		'Sociální oblast': ['SOCI']
+		'Bezpečnost': ['JUST']
+		'Správa a rozvoj území': ['REGI']
+		'Města a bydlení': ['ECON', 'REGI']
+		'Venkov': ['REGI', 'SOCI']
+		'Životní prostředí': ['ENVI']
+		'Příroda a krajina': ['ENVI']
+		'Zemědělství': ['ENVI', 'ECON']
+	} as Record<string, string[]>,
+	periodicity: {
+		'000': 'UNKNOWN',
+		'001': 'CONT',
+		'002': 'DAILY',
+		'003': 'WEEKLY',
+		'004': 'BIWEEKLY',
+		'005': 'MONTHLY',
+		'006': 'QUARTERLY',
+		'007': 'BIENNIAL',
+		'008': 'ANNUAL',
+		'009': 'OTHER',
+		'010': 'IRREG',
+		'011': 'NEVER',
+		'012': 'UNKNOWN',
+	} as Record<string, string>,
+	excludedDatasets: [
+		 '55bde3ffb8814807a81b3e733cd0f66d', // Úřední deska Královéhradeckého kraje
+		 '9cef37eacc0541d89acb3f21481c4809', // Aktuality Královéhradeckého kraje
+	]
+}
+
